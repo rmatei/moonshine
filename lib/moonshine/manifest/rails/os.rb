084e8402e12b1d86f59655f08dd1cb0556104c3a
Randal Truong <randal.truong@gmail.com>
2009-05-08 03:54:32 -0700

    Moving cron job again

--- lib/moonshine/manifest/rails/os.rb
+++ lib/moonshine/manifest/rails/os.rb
@@ -14,13 +14,6 @@ module Moonshine::Manifest::Rails::Os
   def cron_packages
     service "cron", :require => package("cron"), :ensure => :running
     package "cron", :ensure => :installed
-    
-    safename = "#{configuration[:deploy_to]}/shared/log/*.log".gsub(/[^a-zA-Z]/, '')
-    
-    cron :rotate_railslog, 
-      :command => "/usr/sbin/logrotate -f /etc/logrotate.d/#{safename}.conf",
-      :user => 'root',
-      :minute => 15
   end
 
   # Create a MOTD to remind those logging in via SSH that things are managed
