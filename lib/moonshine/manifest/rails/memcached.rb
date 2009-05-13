0629f13b83891244be05436c07a9746aff2cbb20
Randal Truong <randal.truong@gmail.com>
2009-05-12 17:03:07 -0700

    Add telnet package for memcached

--- lib/moonshine/manifest/rails/memcached.rb
+++ lib/moonshine/manifest/rails/memcached.rb
@@ -4,6 +4,9 @@ module Moonshine::Manifest::Rails::Memcached
     package 'memcached', :ensure => :installed 
     service 'memcached', :ensure => :running, :enable => true, :require => package('memcached')
     
+    #need telnet to check memcache stats
+    package 'telnet', :ensure => installed
+    
     file '/etc/memcached.conf',
       :ensure => :present,
       :content => template(File.join(File.dirname(__FILE__), 'templates', 'memcached.conf.erb')),
