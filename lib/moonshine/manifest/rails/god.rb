04bf81b85a11d8fd6085797d04582db071e0596c
Randal Truong <randal.truong@gmail.com>
2009-05-27 17:24:19 -0700

    * Add cap recipes for dj * Add init.d script for god * Add god gem to rake task

--- lib/moonshine/manifest/rails/god.rb
+++ lib/moonshine/manifest/rails/god.rb
@@ -4,6 +4,10 @@ module Moonshine::Manifest::Rails::God
     file '/etc/god.conf',
       :ensure => :present,
       :content => template(File.join(File.dirname(__FILE__), 'templates', 'god.conf.erb'))
+    
+    file '/etc/init.d/god.conf',
+      :ensure => :present,
+      :content => template(File.join(File.dirname(__FILE__), 'templates', 'god.conf.erb'))
   end
   
 
