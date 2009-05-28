36f94048b651c7ceb9d4c30f43edca46644051a5
Randal Truong <randal.truong@gmail.com>
2009-05-27 22:42:00 -0700

    Remove comma

--- lib/moonshine/manifest/rails/god.rb
+++ lib/moonshine/manifest/rails/god.rb
@@ -8,7 +8,7 @@ module Moonshine::Manifest::Rails::God
     file '/etc/init.d/god',
       :ensure => :present,
       :content => template(File.join(File.dirname(__FILE__), 'templates', 'god.init.erb')),
-      :mode => '755',
+      :mode => '755'
     
     exec ("start_on_boot"),
       :command => "/usr/sbin/update-rc.d -f god defaults"
