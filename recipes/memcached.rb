da53497841eb491ff696cde5c8bb01d571cfa887
Robert Cezar Matei <rmatei@gmail.com>
2009-09-15 14:20:58 -0700

    Task to show number of connections to memcached servers

--- recipes/memcached.rb
+++ recipes/memcached.rb
@@ -3,4 +3,9 @@ namespace :memcached do
   task :restart, :roles => :app do
     sudo "sudo /etc/init.d/memcached restart"
   end
+
+  desc "Show number of connections to Memcached"
+  task :total_connections, :roles => :app do
+    run "(sleep 1; echo stats; echo quit) | nc 127.0.0.1 11211 | grep total_connections"
+  end
 end
