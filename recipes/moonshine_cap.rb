7c8e351d8ec51767b36f1c48d20b2f0c48efd9e0
Danny Lee <danny@animalapps.com>
2009-11-24 23:59:46 -0800

    Last deploy tag.

--- recipes/moonshine_cap.rb
+++ recipes/moonshine_cap.rb
@@ -246,10 +246,10 @@ end
 
 # Our additions...
 
-# after "deploy:update_code", "tag_last_deploy"
-# after "deploy:update_code", "newrelic:notice_deployment"
+after "deploy:update_code", "tag_last_deploy"
+after "deploy:update_code", "newrelic:notice_deployment"
 after "deploy:restart", "dj:restart"
-# after "deploy", "deploy:restart"
+after "deploy", "deploy:restart"
 
 namespace :status do
   desc "Tail production log file" 
